1e6248e0a0c1369545771dccfd0def94f888b0a8
Naveen <104185866+naveen67622@users.noreply.github.com>
2022-06-08 15:41:20 +0530

    Add files via upload

---
+++ src/main/java/com/zensar/test/app/Demo.java
@@ -0,0 +1,24 @@
+package com.zensar.test.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	public Demo() {
+		System.out.println("In Constructor");
+	}
+		public void calculateData ()
+		{
+			String s = "";
+			List<Integer> list = new ArrayList<Integer>();
+			list.add(1);
+			list.add(2);
+
+
+			Object obj = null;
+			System.out.println(obj.toString());
+		}
+
+	}
+
+
